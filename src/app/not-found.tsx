c6c71fcff914f9b598d5fe128579f70abd8ae86f
Brian Espinosa <b@bje.co>
2025-03-11 08:05:03 -0700

    setup DX

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from 'next';
+import NextLink from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Not Found',
+};
+
+export default function NotFound() {
+  return (
+    <main>
+      <h2>Not Found</h2>
+      <p>This is not the page you&apos;re looking for...</p>
+      <NextLink href='/'>Go back home</NextLink>
+    </main>
+  );
+}
